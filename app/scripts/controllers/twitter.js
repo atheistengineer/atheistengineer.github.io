3a48df3a6c2db4286fa8fa993802363b7e0574c8
Atheist Engineer <atheistengineer4@gmail.com>
2016-05-30 21:42:36 -0400

    updated version of the website.  Now live.

--- app/scripts/controllers/twitter.js
+++ app/scripts/controllers/twitter.js
@@ -8,15 +8,34 @@
  * Controller of the atheistengineergithubioApp
  */
 angular.module('atheistengineergithubioApp')
-  .controller('TwitterCtrl', function ($scope, $location, $routeParams) {
+  .controller('TwitterCtrl', function ($scope, $routeParams) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    $scope.ref=$routeParams.ref;
+    $scope.timelines = {
+        'me': ['My Tweets', '737369040692367360'],
+        'celebrities': ['Atheist Celebrities', '737415513077362688'],
+        'evolution': ['Evolution', '737416906966204416'],
+        'organizations': ['Organizations', '737415726059917316'],
+        'podcasts': ['Smart Podcasts', '737412950714134528'],
+        'space': ['Space Content', '737415216791707648'],
+        'science-deniers': ['Science Deniers', "737409885277683716"], 
+        'scientists': ['Scientists', '737412701144666112'],
+    };
+    var this_ref = $routeParams.ref;
+    if ($scope.timelines[this_ref] === undefined) {
+        this_ref = 'me';
+    }
+    $scope.timeline_id = $scope.timelines[this_ref][1];
 	$scope.isActive = function (ref) { 
-        return ref === $routeParams.ref;
+        return ref === this_ref;
     };
     
   });
+
+
+/*
+<a class="twitter-timeline" href="https://twitter.com/AtheistEngineer/lists/podcasts" data-widget-id="737409885277683716">Tweets from https://twitter.com/AtheistEngineer/lists/podcasts</a>
+*/
