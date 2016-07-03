7e3dcde98a372b73aa6b8a2ec469b16d7f2d09e1
Atheist Engineer <atheistengineer4@gmail.com>
2016-07-03 15:12:17 -0400

    working slugs and titles.

--- app/scripts/controllers/reason.js
+++ app/scripts/controllers/reason.js
@@ -32,150 +32,169 @@
  2-element truth table on the edge?
  */
 angular.module('atheistengineergithubioApp')
-  .controller('ReasonCtrl', ['$scope', '$routeParams', '$location', '$firebaseArray',
-  '$firebaseObject', 'Auth', 'Reason', 'VisDataSet', 'Firebase',
-  function ($scope, $routeParams, $location, $firebaseArray,
-            $firebaseObject, Auth, Reason, VisDataSet, Firebase) {
+.controller('ReasonCtrl', ['$scope', '$routeParams', '$location', '$firebaseArray',
+'$firebaseObject', 'Auth', 'Reason', 'VisDataSet', 'Firebase',
+function ($scope, $routeParams, $location, $firebaseArray,
+          $firebaseObject, Auth, Reason, VisDataSet, Firebase) {
 
-    $scope.auth = Auth;
-    $scope.firebaseUser =  {}
-    // From https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
-    var ref = firebase.database().ref();
-    // var array = $firebaseArray(ref);
-      // download the data into a local object
-    //var graph = $firebaseObject(ref.child("claims").child('single-claim'));
-    var graph = Reason('', $scope.auth.uid)
-    $scope.graph = {
-      'owner': $scope.auth.uid
-    };
+  $scope.auth = Auth;
+  $scope.firebaseUser =  {}
+  // From https://github.com/firebase/angularfire/blob/master/docs/quickstart.md
+  var ref = firebase.database().ref();
+  // var array = $firebaseArray(ref);
+    // download the data into a local object
+  //var graph = $firebaseObject(ref.child("claims").child('single-claim'));
 
-    // synchronize the object with a three-way data binding
-    // click on `index.html` above to see it used in the DOM!
-    graph.$bindTo($scope, "graph");
+  if ($routeParams.slug !== undefined) {
+    var slug = slugify($routeParams.slug);
+  } else {
+    var slug = '';
+  }
 
+  var graph = Reason(slug, $scope.auth.uid)
+  $scope.graph = {
+    'owner': $scope.auth.uid
+  };
 
-    // any time auth state changes, add the user data to scope
-    $scope.auth.$onAuthStateChanged(function(firebaseUser) {
-      console.log("User Auth State Changed", firebaseUser);
-      if (firebaseUser !== null) {
-        if ($scope.graph.owner === undefined) {
-          console.log('Updated graph owner')
-          $scope.graph.owner = firebaseUser.uid;
-        }
-      }
-      $scope.firebaseUser = firebaseUser;
-    });
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  graph.$bindTo($scope, "graph");
 
-    var nodes = new VisDataSet();
-    var edges = new VisDataSet();
-
-    // When nodes or edges change, update $scope.graph appropriately.
-    nodes.on('*', function() {
-      $scope.graph.nodes = nodes.get();
-    });
-    edges.on('*', function() {
-      $scope.graph.edges = edges.get();
-    });
-
-    $scope.graphOptions = {
-      'interaction': {
-        'selectConnectedEdges': false,
-        'hover': false
-      },
-      'nodes': {
-        'shape': 'box'
-      }
-    };
-    $scope.graphData = {
-      nodes: nodes,
-      edges: edges
-    };
-
-    $scope.updateNode = function(node){
-      $scope.graphData.nodes.update(node);
-    };
-
-    $scope.selectedNodes = [];
-    $scope.selectedEdges = [];
-
-    $scope.supportingNodes = [];
-    $scope.supportedNodes = [];
-    $scope.selectedNodeObjects = [];
-
-    $scope.supportingNode = [];
-    $scope.supportedNode = [];
-
-    $scope.search = function(query){
-      $location.search(query);
-    };
-
-    /* This function is called when something on the graph is selected. It
-     updates the data models in angular to match the data in the graph.
-    */
-    function graphSelect(ev, noapply){
-      if (ev.nodes === undefined) {
-        ev.nodes = [];
-      }
-      $scope.selectedNodes = ev.nodes;
-      $scope.selectedNodeObjects = $scope.graphData.nodes.get(ev.nodes);
-
-      if (ev.edges === undefined){
-        ev.edges = [];
-      }
-      $scope.selectedEdges = ev.edges;
-      $scope.selectedEdgeObjects = $scope.graphData.edges.get(ev.edges);
-
-      $location.search({edge:ev.edges, node:ev.nodes});
-
-      if (ev.edges.length > 0){
-          $scope.supportedNode = $scope.graphData.nodes.get(ev.edges[0].to);
-          $scope.supportingNode = $scope.graphData.nodes.get(ev.edges[0].from);
-      }
-
-      if (ev.nodes.length > 0) {
-        /* Update SupportingNodes */
-        var supporting_edges = $scope.graphData.edges.get({
-          filter: function (item) {
-            return (ev.nodes.indexOf(item.to.toString()) >= 0);
-          }
-        });
-
-        var from_ids = $.map(supporting_edges, function(e){return e.from;});
-        $scope.supportingNodes = $scope.graphData.nodes.get(from_ids);
-
-        /* Update SupportedNodes */
-        var supported_edges = $scope.graphData.edges.get({
-          filter: function (item) {
-            return (ev.nodes.indexOf(item.from.toString()) >= 0);
-          }
-        });
-
-        var to_ids = $.map(supported_edges, function(e){return e.to;});
-        $scope.supportedNodes = $scope.graphData.nodes.get(to_ids);
-      } else {
-        $scope.supportedNodes = [];
-        $scope.supportingNodes = [];
-      }
-
-      if(noapply !== true) {
-        $scope.$apply();
+  // any time auth state changes, add the user data to scope
+  $scope.auth.$onAuthStateChanged(function(firebaseUser) {
+    console.log("User Auth State Changed", firebaseUser);
+    if (firebaseUser !== null) {
+      if ($scope.graph.owner === undefined) {
+        console.log('Updated graph owner')
+        graph.owner = firebaseUser.uid;
+        graph.$save();
       }
     }
+    $scope.firebaseUser = firebaseUser;
+  });
 
-    $scope.removeEdge = function(edge){
-      $scope.graphData.edges.remove(edge.id);
-      graphSelect({nodes:[], edges:[]}, true);
-    };
+  var nodes = new VisDataSet();
+  var edges = new VisDataSet();
 
-    $scope.removeNode = function(node){
-      $scope.graphData.nodes.remove(node.id);
-      var bad_edges = $scope.graphData.edges.get({
+  // When nodes or edges change, update $scope.graph appropriately.
+  nodes.on('*', function() {
+    $scope.graph.nodes = nodes.get();
+  });
+  edges.on('*', function() {
+    $scope.graph.edges = edges.get();
+  });
+
+  $scope.graphOptions = {
+    'interaction': {
+      'selectConnectedEdges': false,
+      'hover': false
+    },
+    'nodes': {
+      'shape': 'box'
+    }
+  };
+  $scope.graphData = {
+    nodes: nodes,
+    edges: edges
+  };
+
+  $scope.setSlug = function (name) {
+    // For some reason, setting $scope.graph.xxx works here, but we have to
+    // set graph.xxx other places. graph.xxx doesn't seem to work here.
+    $scope.graph.slug = name.toString().toLowerCase()
+      .replace(/\s+/g, '-')           // Replace spaces with -
+      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+      .replace(/^-+/, '')             // Trim - from start of text
+      .replace(/-+$/, '');            // Trim - from end of text
+  };
+
+  $scope.updateNode = function(node){
+    $scope.graphData.nodes.update(node);
+  };
+
+  $scope.selectedNodes = [];
+  $scope.selectedEdges = [];
+
+  $scope.supportingNodes = [];
+  $scope.supportedNodes = [];
+  $scope.selectedNodeObjects = [];
+
+  $scope.supportingNode = [];
+  $scope.supportedNode = [];
+
+  $scope.search = function(query){
+    $location.search(query);
+  };
+
+
+  /* This function is called when something on the graph is selected. It
+   updates the data models in angular to match the data in the graph.
+  */
+  function graphSelect(ev, noapply){
+    if (ev.nodes === undefined) {
+      ev.nodes = [];
+    }
+    $scope.selectedNodes = ev.nodes;
+    $scope.selectedNodeObjects = $scope.graphData.nodes.get(ev.nodes);
+
+    if (ev.edges === undefined){
+      ev.edges = [];
+    }
+    $scope.selectedEdges = ev.edges;
+    $scope.selectedEdgeObjects = $scope.graphData.edges.get(ev.edges);
+
+    $location.search({edge:ev.edges, node:ev.nodes});
+
+    if (ev.edges.length > 0){
+        $scope.supportedNode = $scope.graphData.nodes.get(ev.edges[0].to);
+        $scope.supportingNode = $scope.graphData.nodes.get(ev.edges[0].from);
+    }
+
+    if (ev.nodes.length > 0) {
+      /* Update SupportingNodes */
+      var supporting_edges = $scope.graphData.edges.get({
         filter: function (item) {
-          return (item.to === node.id) || (item.from === node.id);
-        }});
-      $scope.graphData.edges.remove(bad_edges);
-      graphSelect({nodes:[], edges:[]}, true);
-    };
+          return (ev.nodes.indexOf(item.to.toString()) >= 0);
+        }
+      });
+
+      var from_ids = $.map(supporting_edges, function(e){return e.from;});
+      $scope.supportingNodes = $scope.graphData.nodes.get(from_ids);
+
+      /* Update SupportedNodes */
+      var supported_edges = $scope.graphData.edges.get({
+        filter: function (item) {
+          return (ev.nodes.indexOf(item.from.toString()) >= 0);
+        }
+      });
+
+      var to_ids = $.map(supported_edges, function(e){return e.to;});
+      $scope.supportedNodes = $scope.graphData.nodes.get(to_ids);
+    } else {
+      $scope.supportedNodes = [];
+      $scope.supportingNodes = [];
+    }
+
+    if(noapply !== true) {
+      $scope.$apply();
+    }
+  }
+
+  $scope.removeEdge = function(edge){
+    $scope.graphData.edges.remove(edge.id);
+    graphSelect({nodes:[], edges:[]}, true);
+  };
+
+  $scope.removeNode = function(node){
+    $scope.graphData.nodes.remove(node.id);
+    var bad_edges = $scope.graphData.edges.get({
+      filter: function (item) {
+        return (item.to === node.id) || (item.from === node.id);
+      }});
+    $scope.graphData.edges.remove(bad_edges);
+    graphSelect({nodes:[], edges:[]}, true);
+  };
 
   $scope.createNode = function()  {
     var node_id = $scope.graphData.nodes.add({'label': 'New Node'});
